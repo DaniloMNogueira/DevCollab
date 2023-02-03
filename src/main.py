9fba8393ae2b07fe40d18d2aac4c048fcd160525
danilo <danilo@neuralmed.ai>
2023-02-03 13:42:34 -0400

    Initial commit 2

--- src/main.py
+++ src/main.py
@@ -1,10 +1,11 @@
 APP_VERSION = 'v0.1.0'
 
 
-def main(message: str):
+def main():
+    message = 'Olá NeuralMed!!!'
     print(message)
 
 
 if __name__ == '__main__':
-    print(f'App version: {APP_VERSION}')
-    main('Olá NeuralMed!!!')
+    print(f'App version: {APP_VERSION}\n')
+    main()
