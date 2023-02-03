29e3a9e762fad987f301f1030d9279ea4e193baf
danilo <danilo@neuralmed.ai>
2023-02-03 14:16:03 -0400

    Implementa a primeira Feature

--- src/main.py
+++ src/main.py
@@ -1,11 +1,30 @@
+from random import randint
+
 APP_VERSION = 'v0.1.0'
+
+
+def get_happy_messages():
+    messages = [
+        'O importante não é ganhar, o que importa é competir sem perder, nem empatar.',
+        'Sacode a poeira e dá a volta por cima! Só tome cuidado com a rinite.',
+        'Batatinha quando nasce se esparrama pelo chão, agir é mais importante do que ter motivação.',
+        'O Google Maps não pode te ajudar a encontrar o rumo da sua vida. Descubra!',
+        'Que seu dia seja Ben 10 e você não desanime.',
+        'Quando te chamarem de amador, lembre-se: amadores fizeram a arca de Noé e profissionais fizeram o Titanic.',
+        'Corra atrás dos seus sonhos com a mesma pressa que você corre atrás do busão.',
+    ]
+
+    return messages[randint(0, len(messages))]
 
 
 def main():
     message = 'Olá NeuralMed!!!'
     print(message)
+    happy_message = get_happy_messages()
+    print(happy_message)
 
 
 if __name__ == '__main__':
     print(f'App version: {APP_VERSION}\n')
     main()
+
